ed1c207a0101c8ad7d668a7db4f3f4ed0c9a3288
pedrotoledos <phtoledo@gmail.com>
2021-07-17 18:12:11 -0300

    java

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -2,26 +2,28 @@ package com.dio;
 
 import com.dio.model.gato;
 
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		
-		gato gato = new Gato();
+
+		gato Gato = new gato();
 		livros livros = new livros();
-		
-				System.out.println(gato);
+
+		System.out.println(Gato);
 		System.out.println(livros);
-		
-				
-				/*int a = 2;
-                  int b =3;
-		System.out.println("hello world!" + (a+b));
-		*/
+
+		/*
+		 int a = 2; 
+		 int b =3; 
+		 System.out.println("hello world!" + (a+b));
+		 */
 	}
 
 }
+
 class livros {
 	private String nome;
 	private String npag;
-	
+
 }
