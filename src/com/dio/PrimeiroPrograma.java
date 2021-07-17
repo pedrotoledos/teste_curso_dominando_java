d8413427c513fcb92abae667c38cfa358a90a216
pedrotoledos <phtoledo@gmail.com>
2021-07-17 16:45:17 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package com.dio;
+
+import com.dio.model.gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		gato gato = new Gato();
+		livros livros = new livros();
+		
+				System.out.println(gato);
+		System.out.println(livros);
+		
+				
+				/*int a = 2;
+                  int b =3;
+		System.out.println("hello world!" + (a+b));
+		*/
+	}
+
+}
+class livros {
+	private String nome;
+	private String npag;
+	
+}
